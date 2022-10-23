c4a026da0b5b7e4967ff49e706b05c26ac3d688b
JuanManuelCelisgomez <juacelgom@alum.us.es>
2022-10-23 20:07:16 +0200

    Cambios en la entrega

--- src/Netflix.py
+++ src/Netflix.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 
+''' Este módulo contiene las funciones principales que decriben el programa'''
+
 import csv
 from collections import namedtuple
 from Parsers import *
 from datetime import datetime 
 
+
+''' 
+------------------------------------Entregra 1------------------------------------------------------------
+La primera entrega contiene una función de apertura de fichero csv
+'''
+
 Dats = namedtuple('Dats', 'type,title,director,country,co_production,date_added,release_year,duration,description')
 
 def abrir_ficherocsv (fichero):
-
+    '''
+    @param fichero: fichero contiene los datos del csv
+    @type: str, parseo de fecha(date.time), int
+    @return: Una tupla con todos los datos del csv plataforma_streaming.csv
+    '''
     with open (fichero, encoding='utf-8') as f:
 
         lector = csv.reader(f, delimiter= ";")
--- src/NetflixTest.py
+++ src/NetflixTest.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
+'''
+Este módulo contiene el test del módulo Netflix, actualmente proporciona un error de conversión de datos.
+'''
 from Netflix import *
 
+'''
+-------------------------------------Entrega 1------------------------------------------------------
+'''
+
 def test_abrir_ficherocsv(fichero):
+    '''
+    @param cadena: fichero contiene los datos del csv
+    @return: dos tuplas que contienen los cinco primeros registros y los cinco últimos registros
+    '''
     print ("="*25 + "test_lee_fichero" +"="*25)
     LISTA_TUPLAS = abrir_ficherocsv(fichero)
     print('Leídos', len(LISTA_TUPLAS),'registros.')
     print('Los cinco primeros registros son:',LISTA_TUPLAS[:5])
     print('Los cinco últimos registros son:',LISTA_TUPLAS[-5:])
 
-
+'''
+-------------------------------------------------------------------------------------------------------
+'''
 
 def main():
 
